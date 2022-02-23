993a9b62fcc13905b5403cc5773034acb09dace8
Timothy Gaither <timjgaither@gmail.com>
2022-02-23 15:40:10 +0100

    push header

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,42 @@
+<template>
+<header>
+    <MyLogo  v-for="(logo, index) in logos" 
+            :key="index"
+            :img="logo.src" 
+            :text="logo.titolo" />
+</header>
+  
+</template>
+
+<script>
+
+import MyLogo from "./partials/MyLogo.vue"
+export default {
+    name:"MyHeader",
+
+    components:{
+        MyLogo
+    },
+    data(){
+        return{
+            logos:[
+                {
+                   src:"https://static.movietele.it/files/styles/image-w300/public/images/2022/01/mtv.png",
+                   titolo:"Logo mtv"
+                }
+
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 80px;
+    padding: 10px;
+    display: flex;
+    background-color: #2e3a46;
+}
+
+</style>
---
+++ src/components/partials/MyLogo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img :src="img" :alt="text">
+    </div>
+</template>
+
+<script>
+export default {
+    name:"MyLogo",
+    props:{
+        'img':String,
+        'text':String
+    }
+}
+</script>
+
+<style scoped>
+    img{
+        height: 50px;
+        border-radius: 25px;
+    }
+</style>
